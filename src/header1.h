c484c63b3c8d2a90def0876b156f96f9d6730e7e
Kenta_Nakajima <knknkn1162@gmail.com>
2021-12-25 20:32:27 +0900

    initial commit

---
+++ src/header1.h
@@ -0,0 +1,3 @@
+
+#define EXTERNAL_NUMBER 100
+void print_external(void);
---
+++ src/src1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "header1.h"
+
+void print_external(void) {
+    printf("%d: print_external\n", EXTERNAL_NUMBER);
+}
